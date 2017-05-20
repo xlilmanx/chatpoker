64a5c4eb4f895aaab3ce920059274cb957964f81
xlilmanx <xlilmanx@users.noreply.github.com>
2017-05-20 16:18:30 -0700

    test

--- server/index.js
+++ server/index.js
@@ -41,6 +41,55 @@ io.on('connection', function (socket) {
   socket.on('chat message', function (msg) {
     io.emit('chat message', msg);
   });
+  
+  var name = userNames.getGuestName();
+
+  // send the new user their name and a list of users
+  socket.emit('init', {
+    name: name,
+    users: userNames.get()
+  });
+
+  // notify other clients that a new user has joined
+  socket.broadcast.emit('user:join', {
+    name: name
+  });
+
+  // broadcast a user's message to other users
+  socket.on('send:message', function (data) {
+    socket.broadcast.emit('send:message', {
+      user: name,
+      text: data.text
+    });
+  });
+
+  // validate a user's name change, and broadcast it on success
+  socket.on('change:name', function (data, fn) {
+    if (userNames.claim(data.name)) {
+      var oldName = name;
+      userNames.free(oldName);
+
+      name = data.name;
+      
+      socket.broadcast.emit('change:name', {
+        oldName: oldName,
+        newName: name
+      });
+
+      fn(true);
+    } else {
+      fn(false);
+    }
+  });
+
+  // clean up when a user leaves, and broadcast it to other users
+  socket.on('disconnect', function () {
+    socket.broadcast.emit('user:left', {
+      name: name
+    });
+    userNames.free(name);
+  });
+  
 });
 
 server.listen(PORT, function () {
@@ -99,51 +148,5 @@ var userNames = (function () {
 
 // export function for listening to the socket
 module.exports = function (socket) {
-  var name = userNames.getGuestName();
-
-  // send the new user their name and a list of users
-  socket.emit('init', {
-    name: name,
-    users: userNames.get()
-  });
-
-  // notify other clients that a new user has joined
-  socket.broadcast.emit('user:join', {
-    name: name
-  });
-
-  // broadcast a user's message to other users
-  socket.on('send:message', function (data) {
-    socket.broadcast.emit('send:message', {
-      user: name,
-      text: data.text
-    });
-  });
-
-  // validate a user's name change, and broadcast it on success
-  socket.on('change:name', function (data, fn) {
-    if (userNames.claim(data.name)) {
-      var oldName = name;
-      userNames.free(oldName);
-
-      name = data.name;
-      
-      socket.broadcast.emit('change:name', {
-        oldName: oldName,
-        newName: name
-      });
-
-      fn(true);
-    } else {
-      fn(false);
-    }
-  });
-
-  // clean up when a user leaves, and broadcast it to other users
-  socket.on('disconnect', function () {
-    socket.broadcast.emit('user:left', {
-      name: name
-    });
-    userNames.free(name);
-  });
+  
 };
