f30e441539f3e2eccad3ac573223934360f4aae9
xlilmanx <xlilmanx@users.noreply.github.com>
2017-05-20 04:44:02 -0700

    asdf

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ var app = express();
 var server = http.Server(app);
 var io = require('socket.io')(server);
 var PORT = process.env.PORT || 5000;
+var useronline = 0;
 
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
