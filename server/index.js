50cc5aadea3f7bc4280e23c71302eeb4c9e5687a
xlilmanx <xlilmanx@users.noreply.github.com>
2017-05-20 03:06:43 -0700

    test

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const path = require('path');
 
+
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+var useronline = 0;
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -20,4 +25,30 @@ app.get('*', function(request, response) {
 
 app.listen(PORT, function () {
   console.log(`Listening on port ${PORT}`);
+});
+
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+  io.emit('chat message', 'a user connected');
+  useronline = useronline + 1;
+  io.emit('userupdate', useronline);
+
+  socket.on('disconnect', function () {
+    io.emit('chat message', 'a user disconnected');
+    useronline = useronline - 1;
+  io.emit('userupdate', useronline);
+
+  });
+  socket.on('chat message', function (msg) {
+    io.emit('chat message', msg);
+  });
+});
+
+
+http.listen(process.env.PORT || 8080, function () {
+  console.log('listening on *:5000');
 });
