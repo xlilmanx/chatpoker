2ef032024908996d39ef9937fa0453aedcf8701e
xlilmanx <xlilmanx@users.noreply.github.com>
2017-05-21 00:23:36 -0700

    test

--- react-ui/src/Game.js
+++ react-ui/src/Game.js
@@ -62,7 +62,7 @@ class Game extends React.Component {
   }
 
 
-  /*
+  
     dealHand() {
       var deckarr = ["Ac", "Ad", "Ah", "As", "2c", "2d", "2h", "2s", "3c", "3d", "3h", "3s", "4c", "4d", "4h", "4s",
         "5c", "5d", "5h", "5s", "6c", "6d", "6h", "6s", "7c", "7d", "7h", "7s", "8c", "8d", "8h", "8s",
@@ -100,7 +100,7 @@ class Game extends React.Component {
         deck: deckarr
       });
     }
-  */
+
   handleDealHand() {
 
     this.props.socket.emit('dealhand');
