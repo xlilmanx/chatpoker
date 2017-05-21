f8f16cc94de196fd8f3ee7ac5fea0b576aa6d8ee
xlilmanx <xlilmanx@users.noreply.github.com>
2017-05-21 02:41:19 -0700

    test

--- server/index.js
+++ server/index.js
@@ -28,39 +28,49 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname, '../react-ui/public/index.html');
 });
 */
+var deck = ["Ac", "Ad", "Ah", "As", "2c", "2d", "2h", "2s", "3c", "3d", "3h", "3s", "4c", "4d", "4h", "4s",
+  "5c", "5d", "5h", "5s", "6c", "6d", "6h", "6s", "7c", "7d", "7h", "7s", "8c", "8d", "8h", "8s",
+  "9c", "9d", "9h", "9s", "10c", "10d", "10h", "10s", "Jc", "Jd", "Jh", "Js",
+  "Qc", "Qd", "Qh", "Qs", "Kc", "Kd", "Kh", "Ks"];
+var deckref = ["Ac", "Ad", "Ah", "As", "2c", "2d", "2h", "2s", "3c", "3d", "3h", "3s", "4c", "4d", "4h", "4s",
+  "5c", "5d", "5h", "5s", "6c", "6d", "6h", "6s", "7c", "7d", "7h", "7s", "8c", "8d", "8h", "8s",
+  "9c", "9d", "9h", "9s", "10c", "10d", "10h", "10s", "Jc", "Jd", "Jh", "Js",
+  "Qc", "Qd", "Qh", "Qs", "Kc", "Kd", "Kh", "Ks"];
+
+var field = [];
+//var playerhand = { id: "", hand: "" }
+var allhand = [];
+var returnarray = [];
+var playercount = 0;
+var deckarr = [];
+var fieldarr = [];
+var num1 = 0;
+var card1 = "";
+var num2 = 0;
+var card2 = "";
+var hand = [];
+
+
+
 io.on('connection', function (socket) {
 
   // game stuff
-  var deck = ["Ac", "Ad", "Ah", "As", "2c", "2d", "2h", "2s", "3c", "3d", "3h", "3s", "4c", "4d", "4h", "4s",
-    "5c", "5d", "5h", "5s", "6c", "6d", "6h", "6s", "7c", "7d", "7h", "7s", "8c", "8d", "8h", "8s",
-    "9c", "9d", "9h", "9s", "10c", "10d", "10h", "10s", "Jc", "Jd", "Jh", "Js",
-    "Qc", "Qd", "Qh", "Qs", "Kc", "Kd", "Kh", "Ks"];
-  var deckref = ["Ac", "Ad", "Ah", "As", "2c", "2d", "2h", "2s", "3c", "3d", "3h", "3s", "4c", "4d", "4h", "4s",
-    "5c", "5d", "5h", "5s", "6c", "6d", "6h", "6s", "7c", "7d", "7h", "7s", "8c", "8d", "8h", "8s",
-    "9c", "9d", "9h", "9s", "10c", "10d", "10h", "10s", "Jc", "Jd", "Jh", "Js",
-    "Qc", "Qd", "Qh", "Qs", "Kc", "Kd", "Kh", "Ks"];
 
-  var field = [];
-  //var playerhand = { id: "", hand: "" }
-  var allhand = [];
-  var returnarray = [];
-  var playercount = 0;
 
-  function addPlayer() {
 
-    //    playerhand["id"] = id;
-    //    playerhand["hand"] = hand;
-    //    allhand.push(playerhand);
-    playercount = playercount + 1;
+  //    playerhand["id"] = id;
+  //    playerhand["hand"] = hand;
+  //    allhand.push(playerhand);
+  playercount = playercount + 1;
 
-  }
+
 
 
 
   socket.on('dealhand', function () {
 
 
-    var deckarr = ["Ac", "Ad", "Ah", "As", "2c", "2d", "2h", "2s", "3c", "3d", "3h", "3s", "4c", "4d", "4h", "4s",
+    deckarr = ["Ac", "Ad", "Ah", "As", "2c", "2d", "2h", "2s", "3c", "3d", "3h", "3s", "4c", "4d", "4h", "4s",
       "5c", "5d", "5h", "5s", "6c", "6d", "6h", "6s", "7c", "7d", "7h", "7s", "8c", "8d", "8h", "8s",
       "9c", "9d", "9h", "9s", "10c", "10d", "10h", "10s", "Jc", "Jd", "Jh", "Js", "Qc", "Qd", "Qh", "Qs",
       "Kc", "Kd", "Kh", "Ks"];
@@ -68,14 +78,14 @@ io.on('connection', function (socket) {
 
     for (i = 0; i < playercount.length; i++) {
 
-      var num1 = Math.floor(Math.random() * (deckarr.length - 1));
-      var card1 = deckarr[num1];
+      num1 = Math.floor(Math.random() * (deckarr.length - 1));
+      card1 = deckarr[num1];
       deckarr.splice(num1, 1);
-      var num2 = Math.floor(Math.random() * (deckarr.length - 1));
-      var card2 = deckarr[num2];
+      num2 = Math.floor(Math.random() * (deckarr.length - 1));
+      card2 = deckarr[num2];
       deckarr.splice(num2, 1);
 
-      var hand = [card1, card2];
+      hand = [card1, card2];
 
       allhand.push(hand);
       deck = deckarr;
@@ -91,12 +101,12 @@ io.on('connection', function (socket) {
 
   socket.on('dealfield', function () {
 
-    var deckarr = deck;
-    var num1 = Math.floor(Math.random() * (deckarr.length - 1));
-    var card1 = deckarr[num1];
+    deckarr = deck;
+    num1 = Math.floor(Math.random() * (deckarr.length - 1));
+    card1 = deckarr[num1];
     deckarr.splice(num1, 1);
 
-    var fieldarr = field;
+    fieldarr = field;
     fieldarr.push(card1);
 
     field = fieldarr;
