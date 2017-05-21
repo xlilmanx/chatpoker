58bd6b8c63ba54c866147ddf6108d276b5d3eb77
xlilmanx <xlilmanx@users.noreply.github.com>
2017-05-21 15:04:57 -0700

    test

--- react-ui/src/Game.js
+++ react-ui/src/Game.js
@@ -55,7 +55,10 @@ class Game extends React.Component {
     this.state = {
       hand: [],
       field: [],
-      deck: [],
+      deck: ["Ac", "Ad", "Ah", "As", "2c", "2d", "2h", "2s", "3c", "3d", "3h", "3s", "4c", "4d", "4h", "4s",
+      "5c", "5d", "5h", "5s", "6c", "6d", "6h", "6s", "7c", "7d", "7h", "7s", "8c", "8d", "8h", "8s",
+      "9c", "9d", "9h", "9s", "10c", "10d", "10h", "10s", "Jc", "Jd", "Jh", "Js", "Qc", "Qd", "Qh", "Qs",
+      "Kc", "Kd", "Kh", "Ks"],
       users: []
     };
     this.dealHand = this.dealHand.bind(this);
