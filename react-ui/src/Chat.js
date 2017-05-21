bd23e03cd8f991e60985c708d4b8ade84573792d
xlilmanx <xlilmanx@users.noreply.github.com>
2017-05-20 19:58:53 -0700

    test

--- react-ui/src/Chat.js
+++ react-ui/src/Chat.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import io from 'socket.io-client'
+
 
 var socket = io();
 
--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ChatApp from './Chat.js';
 import Game from './Game.js';
-
+import io from 'socket.io-client'
 
 
 ReactDOM.render(<Game />, document.getElementById('wrapper'));
