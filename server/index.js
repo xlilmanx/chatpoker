7dd7d6bb3e482b7f424e6af633c12f7eb18793e0
xlilmanx <xlilmanx@users.noreply.github.com>
2017-05-21 16:30:13 -0700

    test

--- server/index.js
+++ server/index.js
@@ -206,7 +206,7 @@ io.on('connection', function (socket) {
         gameinprogress = false;
         handdealt = false;
 
-        var winnername = userid[results[0].id-1].id;
+        var winnername = userid[results[0].id - 1].id;
 
         io.emit('send:message', {
           user: "APPLICATION BOT",
@@ -282,10 +282,12 @@ io.on('connection', function (socket) {
       if (c.id == socket.id) {
         userid.splice(i, 1);
 
-        if (returnarray[0][i] != null) {
-          returnarray[0].splice(i, 1);
+        if (returnarray[0] != null) {
+          if (returnarray[0][i] != null) {
+            returnarray[0].splice(i, 1);
+          }
+          break;
         }
-        break;
       }
     }
 
