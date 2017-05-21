a4b3b9dd9fab02f71302c6de6e4749c4f9e13a38
xlilmanx <xlilmanx@users.noreply.github.com>
2017-05-21 02:27:12 -0700

    test

--- server/index.js
+++ server/index.js
@@ -31,18 +31,81 @@ app.get('/', function (req, res) {
 io.on('connection', function (socket) {
 
   // game stuff
-  game.addPlayer;
+  var deck = ["Ac", "Ad", "Ah", "As", "2c", "2d", "2h", "2s", "3c", "3d", "3h", "3s", "4c", "4d", "4h", "4s",
+    "5c", "5d", "5h", "5s", "6c", "6d", "6h", "6s", "7c", "7d", "7h", "7s", "8c", "8d", "8h", "8s",
+    "9c", "9d", "9h", "9s", "10c", "10d", "10h", "10s", "Jc", "Jd", "Jh", "Js",
+    "Qc", "Qd", "Qh", "Qs", "Kc", "Kd", "Kh", "Ks"];
+  var deckref = ["Ac", "Ad", "Ah", "As", "2c", "2d", "2h", "2s", "3c", "3d", "3h", "3s", "4c", "4d", "4h", "4s",
+    "5c", "5d", "5h", "5s", "6c", "6d", "6h", "6s", "7c", "7d", "7h", "7s", "8c", "8d", "8h", "8s",
+    "9c", "9d", "9h", "9s", "10c", "10d", "10h", "10s", "Jc", "Jd", "Jh", "Js",
+    "Qc", "Qd", "Qh", "Qs", "Kc", "Kd", "Kh", "Ks"];
+
+  var field = [];
+  //var playerhand = { id: "", hand: "" }
+  var allhand = [];
+  var returnarray = [];
+  var playercount = 0;
+
+  function addPlayer() {
+
+    //    playerhand["id"] = id;
+    //    playerhand["hand"] = hand;
+    //    allhand.push(playerhand);
+    playercount = playercount + 1;
+
+  }
+
+
 
   socket.on('dealhand', function () {
-    
-    var data = game.dealHand;
-//    io.emit('dealhand', data);
+
+    console.log('testttt')
+    var deckarr = ["Ac", "Ad", "Ah", "As", "2c", "2d", "2h", "2s", "3c", "3d", "3h", "3s", "4c", "4d", "4h", "4s",
+      "5c", "5d", "5h", "5s", "6c", "6d", "6h", "6s", "7c", "7d", "7h", "7s", "8c", "8d", "8h", "8s",
+      "9c", "9d", "9h", "9s", "10c", "10d", "10h", "10s", "Jc", "Jd", "Jh", "Js", "Qc", "Qd", "Qh", "Qs",
+      "Kc", "Kd", "Kh", "Ks"];
+    allhand = [];
+
+    for (i = 0; i < playercount.length; i++) {
+
+      var num1 = Math.floor(Math.random() * (deckarr.length - 1));
+      var card1 = deckarr[num1];
+      deckarr.splice(num1, 1);
+      var num2 = Math.floor(Math.random() * (deckarr.length - 1));
+      var card2 = deckarr[num2];
+      deckarr.splice(num2, 1);
+
+      var hand = [card1, card2];
+
+      allhand.push(hand);
+      deck = deckarr;
+    }
+
+    field = [];
+    returnarray[0] = allhand;
+    returnarray[1] = deck;
+    returnarray[2] = field;
+
+    io.emit('dealhand', returnarray);
   });
 
   socket.on('dealfield', function () {
-    
-    var data = game.dealField;
- //   io.emit('dealfield', data);
+
+    var deckarr = deck;
+    var num1 = Math.floor(Math.random() * (deckarr.length - 1));
+    var card1 = deckarr[num1];
+    deckarr.splice(num1, 1);
+
+    var fieldarr = field;
+    fieldarr.push(card1);
+
+    field = fieldarr;
+    deck = deckarr;
+
+    returnarray[0] = allhand;
+    returnarray[1] = deck;
+    returnarray[2] = field;
+    io.emit('dealfield', returnarray);
   });
 
 
@@ -103,7 +166,16 @@ io.on('connection', function (socket) {
 
   // clean up when a user leaves, and broadcast it to other users
   socket.on('disconnect', function () {
-    game.removePlayer;
+
+    function removePlayer() {
+
+      //    playerhand["id"] = id;
+      //    playerhand["hand"] = hand;
+      //    allhand.push(playerhand);
+      playercount = playercount - 1;
+
+    }
+
     socket.broadcast.emit('user:left', {
       name: name
     });
